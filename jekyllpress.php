dcbeb458d7f6e7c6f1e21971ee988bb34c7e9b90
Jason Stallings <jason@stallin.gs>
2014-11-21 03:32:53 -0600

    Plugin header.
    I just need to get started.

---
+++ jekyllpress.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: JekyllPress
+ * Plugin URI: http://github.com/octalmage/jekyllpress
+ * Description: Manage WordPress posts, Jekyll style! 
+ * Version: 0.1
+ * Author: Jason Stallings
+ * Author URI: http://jason.stallin.gs
+ * License: GPL2
+ */
+
